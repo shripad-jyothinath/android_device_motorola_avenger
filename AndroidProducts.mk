e8eb749e52e1b98d60538cd1beb3a6d44ce3ca89
Shripad <shripad-jyothinath@users.noreply.github.com>
2026-08-24 18:30:45 +0530

    marvel: Add authentic model product definitions
    Define products for marvel codename.
    
    * Targets Motorola Edge 70 Fusion (XT2605 series).
    
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Signed-off-by: Shripad <shripad-jyothinath@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,16 @@
 
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_marvel.mk
+    $(LOCAL_DIR)/lineage_marvel.mk \
+    $(LOCAL_DIR)/evolution_marvel.mk \
+    $(LOCAL_DIR)/orangefox_marvel.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_marvel-user \
     lineage_marvel-userdebug \
-    lineage_marvel-eng
+    lineage_marvel-eng \
+    evolution_marvel-user \
+    evolution_marvel-userdebug \
+    evolution_marvel-eng \
+    orangefox_marvel-eng \
+    orangefox_marvel-userdebug
