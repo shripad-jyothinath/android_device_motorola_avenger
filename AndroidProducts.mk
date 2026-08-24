9022ef43bd6f5ce24d32543d75b9a4c40b44e550
Shripad <shripad-jyothinath@users.noreply.github.com>
2026-08-24 18:30:45 +0530

    avenger: Add authentic model product definitions
    Define products for both avenger and marvel codenames.
    
    * Targets Motorola Edge 70 Fusion (XT2605 series).
    
    Co-authored-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Signed-off-by: Shripad <shripad-jyothinath@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,22 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_avenger.mk
+    $(LOCAL_DIR)/lineage_marvel.mk \
+    $(LOCAL_DIR)/evolution_marvel.mk \
+    $(LOCAL_DIR)/orangefox_marvel.mk \
+    $(LOCAL_DIR)/lineage_avenger.mk \
+    $(LOCAL_DIR)/evolution_avenger.mk \
+    $(LOCAL_DIR)/orangefox_avenger.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_avenger-user \
+    lineage_marvel-user \
+    lineage_marvel-userdebug \
+    lineage_marvel-eng \
+    evolution_marvel-user \
+    evolution_marvel-userdebug \
+    evolution_marvel-eng \
+    orangefox_marvel-eng \
+    orangefox_marvel-userdebug \
     lineage_avenger-userdebug \
-    lineage_avenger-eng
+    evolution_avenger-userdebug \
+    orangefox_avenger-eng
