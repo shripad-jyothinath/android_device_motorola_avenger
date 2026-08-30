7bb56c4dfea8941543bbafa1bad19580b5f64ef4
Shripad <71135716+shripad-jyothinath@users.noreply.github.com>
2026-08-30 16:17:16 +0530

    avenger: Add OrangeFox Recovery configuration and GitHub Actions workflow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_avenger.mk \
-    $(LOCAL_DIR)/evolution_avenger.mk
+    $(LOCAL_DIR)/evolution_avenger.mk \
+    $(LOCAL_DIR)/orangefox_avenger.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_avenger-user \
@@ -14,4 +15,6 @@ COMMON_LUNCH_CHOICES := \
     lineage_avenger-eng \
     evolution_avenger-user \
     evolution_avenger-userdebug \
-    evolution_avenger-eng
+    evolution_avenger-eng \
+    orangefox_avenger-eng \
+    orangefox_avenger-userdebug
---
+++ orangefox_avenger.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2026 The OrangeFox Recovery Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from standard product configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device configuration
+$(call inherit-product, device/motorola/avenger/device.mk)
+
+# Product metadata
+PRODUCT_NAME := orangefox_avenger
+PRODUCT_DEVICE := avenger
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := motorola edge 70 fusion
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
